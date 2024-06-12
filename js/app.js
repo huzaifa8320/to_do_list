80d7901ac63e34c363c641cc6c19863740d21f42
huzaifa8320 <muhammadhuzaifa8320@gmailcom>
2024-06-12 11:31:45 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,39 +1,35 @@
 var input_first = document.getElementById(`input_first`)
 var div_list = document.getElementById(`div_list`)
 var btn_add = document.getElementById(`btn_add`)
-var error = document.getElementById(`error`)
+var btn_dell = document.getElementById(`btn_dell`)
 var count = `0`
 
 function add() {
     if (input_first.value == ``) {
         Swal.fire("Please enter some text ❌");
-       
+
     }
     else {
         ++count
-        var input_value = `${input_first.value.slice(0, 1).toUpperCase()}${input_first.value.slice(1)}`
-        div_list.innerHTML += `<div class='d-flex justify-content-center  gap-2 py-3'><div class='list_item  d-flex align-items-center px-4'><p class='mb-0 list_para'>${count}.  ${input_value}</p></div><button id="edit">Edit</button><button id="delete">Delete</button></div>`
+        var input_first_value = `${input_first.value.slice(0, 1).toUpperCase()}${input_first.value.slice(1)}`
+        div_list.innerHTML += `<div class='d-flex justify-content-center  gap-2 py-3'><div class='list_item  d-flex align-items-center px-4'><p class='mb-0 list_para' id='para'>${count}.  ${input_first_value}</p></div><button class="edit">Edit</button><button class="delete">Delete</button></div>`
         input_first.value = ``
     }
 
     div_list.addEventListener(`click`, function (e) {
-        if (e.target.id ==`edit`) {
-            // e.target.parentElement.parentElement.parentElement.innerHTML = `<div class='d-flex justify-content-center  gap-2 py-3'><div class='list_item  d-flex align-items-center px-4'><input type="text" id="input_second" placeholder="Enter text"></div> <button id = 'save'>Save</button></div>`
-            e.target.parentElement.parentElement.innerHTML = `<div class='d-flex justify-content-center  gap-2 py-3'><div class='list_item  d-flex align-items-center px-4'><input type="text" id="input_second" placeholder="Enter text"></div><button id="edit">Edit</button><button id="delete">Delete</button></div>`
-            var input_second= document.getElementById(`input_second`)
+        if (e.target.classList.contains(`edit`)) {
+            e.target.parentElement.innerHTML = `<div class='list_item  d-flex align-items-center px-4'><input type="text" id="input_second" placeholder="Enter text"></div><button id='save'>Save</button>`
+            var input_second = document.getElementById(`input_second`)
+           
             var save = document.getElementById(`save`)
             function edit() {
                 if (input_second.value == ``) {
-                    error.innerText = `Please enter some text ❌`
-                    divE.classList.add(`divE`)
-                    setTimeout(() => {
-                        error.innerText = ``
-                        divE.classList.remove(`divE`)
-                    }, 3000);
+                    Swal.fire("Please enter some text ❌");
                 }
                 else {
-                    input_value = `${input_second.value.slice(0, 1).toUpperCase()}${input_second.value.slice(1)}`
-                    input_second.parentElement.parentElement.innerHTML = `<div class='d-flex justify-content-center  gap-2 py-3'><div class='list_item  d-flex align-items-center px-4'><p class='mb-0 list_para'>${count}.  ${input_value}</p></div><button id="edit">Edit</button><button id="delete">Delete</button></div>`
+                    ++count
+                    var input_second_value = `${input_second.value.slice(0, 1).toUpperCase()}${input_second.value.slice(1)}`
+                    input_second.parentElement.parentElement.innerHTML = `<div class='list_item  d-flex align-items-center px-4'><p class='mb-0 list_para'>${count}.  ${input_second_value}</p></div><button class="edit">Edit</button><button class="delete">Delete</button>`
                 }
             }
             input_second.addEventListener(`keypress`, function (e) {
@@ -45,8 +41,8 @@ function add() {
                 edit()
             })
         }
-        else if (e.target.classList.contains(`delete`)) {
-            e.target.parentElement.parentElement.remove()
+        else if (e.target.classList.contains('delete')) {
+            e.target.parentElement.remove()
         }
     })
 
@@ -65,11 +61,15 @@ input_first.addEventListener(`keypress`, function (e) {
 })
 
 // activ input when I press i
+
 document.addEventListener('keydown', function (e) {
     if (e.ctrlKey && e.key == `i`) {
         input_first.focus();
     }
 })
 
-
+btn_dell.addEventListener(`click` , function(e){
+    div_list.innerHTML = ``
+    count = 0
+})
 
